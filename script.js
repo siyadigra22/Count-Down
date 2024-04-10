c7dfffef22e973d23cd2825807535be8a197f901
siya <118165565+siyadigra22@users.noreply.github.com>
2024-04-10 22:43:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+// Display session time
+let session = document.getElementById('session').value;
+let sessionTime = `${session}:00`;
+document.getElementById('display').innerHTML = sessionTime;
+
+ // Store and display session time change
+document.getElementById('session').addEventListener('input', function () {
+  clearInterval(timerInterval);
+  session = document.getElementById('session').value;
+  document.getElementById('display').innerHTML = sessionTime;
+});
+
+// Reset timer
+document.getElementById('reset').addEventListener('click', function() {
+    clearInterval(timerInterval);
+    document.getElementById('display').innerHTML = sessionTime;
+});
+
+// Stop Pomodoro on click
+document.getElementById('display').addEventListener('click', function() {
+    clearInterval(timerInterval);
+    document.getElementById('display').innerHTML = sessionTime;
+});
+
+// Run Pomodoro on click
+let timerInterval;
+document.getElementById('display').addEventListener('click', function() {
+  clearInterval(timerInterval);
+
+  // Countdown timer
+  const start = Date.parse(new Date());
+  const end = session*1000*60+start;
+  timerInterval = setInterval(() => {
+    const now = Date.parse(new Date());
+    const t = end - now;
+    const d = Math.floor(t / (1000 * 60 * 60 * 24));
+    const h = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const m = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    const s = Math.floor((t % (1000 * 60)) / 1000);
+
+    d > 0 ? document.getElementById('display').innerHTML = `Reduce session length`
+      : h > 0 && m < 10 && s < 10 ? document.getElementById('display').innerHTML = `${h}:0${m}:0${s}`
+      : h > 0 && m < 10 ? document.getElementById('display').innerHTML = `${h}:0${m}:${s}`
+      : h > 0 && s < 10 ? document.getElementById('display').innerHTML = `${h}:${m}:0${s}`
+      : h > 0 ? document.getElementById('display').innerHTML = `${h}:${m}:${s}`
+      : m < 10 && s < 10 ? document.getElementById('display').innerHTML = `0${m}:0${s}` 
+      : m < 10 ? document.getElementById('display').innerHTML = `0${m}:${s}` 
+      : s < 10 ? document.getElementById('display').innerHTML = `${m}:0${s}` 
+      : document.getElementById('display').innerHTML = `${m}:${s}`;
+
+    if (t <= 0) {
+      clearInterval(timerInterval);
+      document.getElementById('play').play();
+      document.addEventListener('click', function () {
+        document.getElementById('play').pause()
+        });
+    }
+}, 1000);
+});
